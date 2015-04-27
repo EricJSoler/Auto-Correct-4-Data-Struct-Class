eea03738b1261849700cd4e53a11196b1fb587a2
Eric Soler <e.j.soler1@gmail.com>
2015-04-27 12:33:07 -0700

    Adding some more usability
    There is now a search function that saves all the words that follow
    after a string of letters is recieved

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace yo
+﻿namespace WindowsFormsApplication1
 {
     partial class Form1
     {
@@ -28,22 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListBox Suggestions;
             this.textBox1 = new System.Windows.Forms.TextBox();
+            Suggestions = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(72, 80);
+            this.textBox1.Location = new System.Drawing.Point(83, 76);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Suggestions
+            // 
+            Suggestions.AccessibleName = "Selection";
+            Suggestions.AllowDrop = true;
+            Suggestions.FormattingEnabled = true;
+            Suggestions.Location = new System.Drawing.Point(83, 92);
+            Suggestions.Name = "Suggestions";
+            Suggestions.Size = new System.Drawing.Size(100, 95);
+            Suggestions.TabIndex = 1;
+            Suggestions.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(Suggestions);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Trie.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WindowsFormsApplication1
+{
+    public class Trie
+    {
+        public List<string> potentialWord;
+
+        public Trie()
+        {
+            root = new node();
+            root.value = '~';
+            root.children = new List<node>();
+            potentialWord = new List<string>();
+        }
+
+
+
+
+        public struct node
+        {
+            public char value;
+            public List<node> children;
+            public bool flag;
+        }
+
+
+        public node start(string recieved)
+        {
+            
+            node nextPtr = root;
+            potentialWord.Add(recieved);
+            for (int i = 0; i < recieved.Length; i++)
+            {
+                nextPtr = search(nextPtr, recieved[i]);
+            }
+            if (nextPtr.value == root.value)
+            {
+                nextPtr = new node();
+                nextPtr.value = '!';
+            }
+            else
+            {
+                printWordsFromHereOn(nextPtr, 0);    
+            }
+
+
+            return nextPtr;
+
+        }
+
+        private void printWordsFromHereOn(node startingLocation, int wordLocation)
+        {
+            for (int i = 0; i < (startingLocation.children.Count - 1);i++ )
+            {
+                potentialWord.Add(potentialWord[wordLocation]);
+            }
+
+                for (int i = 0, k= 0; i < startingLocation.children.Count; i++, k++)
+                {
+                    if (!(startingLocation.flag))
+                    {
+                        potentialWord[wordLocation + k] += startingLocation.children[i].value;//
+                        printWordsFromHereOn(startingLocation.children[i], wordLocation + k);//testing this with ks
+                    }
+                    else
+                    {
+                        potentialWord.Add(potentialWord[wordLocation + k]);
+                        k++;
+                        potentialWord[wordLocation + k] += startingLocation.children[i].value;
+                        printWordsFromHereOn(startingLocation.children[i], wordLocation + k);
+                        Console.WriteLine("blah");
+                    }
+                }
+            //foreach (node element in startingLocation.children)
+            //{
+            //    potentialWord[wordLocation] += element.value;
+            //    if (element.flag == true && startingLocation.children.Count > 1)
+            //    {
+
+            //        potentialWord.Add(potentialWord[wordLocation]);
+            //        printWordsFromHereOn(startingLocation.children[i], (wordLocation + 1));
+            //    }
+            //    else
+            //        printWordsFromHereOn(element, wordLocation);
+            //    i++;
+            //}
+
+        //    return new node;
+        }
+
+        private void lookForMore(node t)
+        {
+
+        }
+
+        private node search(node n, char letter)
+        {
+            foreach (node element in n.children)
+            {
+                if (element.value == letter)
+                {
+                    return element;
+                }
+            }
+            return n;
+        }
+
+        public void insert(string w)
+        {
+
+            node nextPtr = root;
+            for (int i = 0; i < w.Length; i++)
+            {
+                bool temp = false;
+                if (i == w.Length - 1)
+                    temp = true;
+                char x = w[i];
+                nextPtr = insert(ref nextPtr, x, temp);
+            }
+        }
+
+        private node insert(ref node n, char letter, bool end)
+        {
+            for (int i = 0; i < n.children.Count; i++)
+            {
+                if (n.children[i].value == letter)
+                {
+                    if (!n.children[i].flag && end)//stupid little work around cuz struct stuff in c#
+                    {
+                        node temp = new node();
+                        temp.children = n.children;
+                        temp.value = n.value;
+                        temp.flag = end;
+                        n = temp;
+                    }
+                    return n.children[i];
+                }
+            }
+
+            node newNode = new node();
+            newNode.value = letter;
+            newNode.flag = end;
+            newNode.children = new List<node>();
+            n.children.Add(newNode);
+            return newNode;
+        }
+
+        private node root;
+
+    }
+}
