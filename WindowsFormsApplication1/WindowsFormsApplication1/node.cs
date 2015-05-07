b34e461deeef1826140d266acbf8ab97e5534951
Eric Soler <e.j.soler1@gmail.com>
2015-05-06 22:23:19 -0700

    Fixed the end of word flag
    I fixed the end of word flag but the output is still broken. It has to
    do with the recursive loop through the printFromHereOn function in the
    class Trie

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class node
+    {
+        public node()
+        {
+
+        }
+
+        public char value;
+        public List<node> children;
+        public bool flag;
+
+    }
+}
